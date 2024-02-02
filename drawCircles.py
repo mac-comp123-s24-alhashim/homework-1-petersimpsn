84b5518e9e342137a2ca7ec72ebbbd35131fd496
petersimpsn <157054511+petersimpsn@users.noreply.github.com>
2024-02-01 22:39:07 -0600

    Circle Code

--- drawCircles.py
+++ drawCircles.py
@@ -1,7 +1,7 @@
 """
-Draws six circles, centered on the origin, each a different color.
-Assumption: the number of colors in the turtle colors list is at least
-as long as the number of rings.
+The program draws 6 circles of different colors around each other.
+
+@author: Peter J. Simpson (psimpson@macalester.edu)
 """
 
 
@@ -9,17 +9,17 @@ import turtle
 import math
 
 scr = turtle.Screen()
-turt = turtle.Turtel()
+turt = turtle.Turtle() #The code references "Turtel", when it should be "Turtle"
 
 scr.bgcolor("seashell")
 
-tColors = ["light salmon", "light sky blue", "pale green", "liht coral", "pale turquoise", "plum"]
+tColors = ["light salmon", "light sky blue", "pale green", "light coral", "pale turquoise", "plum"]
 
 turt.width(5)
 numRings = 6
 
 for i in range(numRings):
-    turt.color(tColors[0])
+    turt.color(tColors[i-1]) #The tColors[0] should be tColors[i-1] so it changes with each iteration of the for loop
     radius = 40 * (i + 1)
 
     
@@ -28,10 +28,10 @@ for i in range(numRings):
     turt.down()
     
     turt.left(90)
-    turt.circle(i)
-    turt.right(60)
+    turt.circle(radius) #"turt.circle(i)" makes the radius 1-6, instead of the radius 40*(1-6)
+    turt.right(90) #The turtle was turning 60 instead of 90, making all of the rings off-center
     
-    turt.up
+    turt.up() # the turt.up command was missing the "()" at the end
     turt.backward(radius)
     turt.down()
 
